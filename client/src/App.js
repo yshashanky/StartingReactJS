a02b53bc6c92b1a535473bc6c1de64cd156f5507
yshashanky <812shanky@gmail.com>
2022-05-29 21:50:36 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import CreatePost from './pages/CreatePost'
+import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
+import Registration from './pages/Registartion';
+import Login from './pages/Login';
 
 function App() {
 
@@ -12,11 +14,15 @@ function App() {
       <div className="navbar">
           <Link to="/"> Home Page</Link>
           <Link to="/createpost"> Create A Post</Link>
+          <Link to="/login"> Login </Link>
+          <Link to="/registration"> Registration </Link>
         </div>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/createpost" element={<CreatePost/>} />
           <Route path='/post/byId/:id' element={<Post/>} />
+          <Route path="/registration" element={<Registration/>} />
+          <Route path='/login' element={<Login/>} />
         </Routes>
       </Router>
     </div>
