dda3ea3bbdce5a4dbef9bd3a7cfbc7556f6162f5
yshashanky <812shanky@gmail.com>
2022-06-06 23:40:13 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -20,9 +21,12 @@ app.use("/auth", usersRouter);
 const likesRouter = require('./routes/likes');
 app.use("/likes", likesRouter);
 
-db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
+db.sequelize.sync()
+.then(() => {
+    app.listen(process.env.PORT || 3001, () => {
         console.log("Server running on port 3001");
     });    
+})
+.catch((err) => {
+    console.log(err);
 });
-
